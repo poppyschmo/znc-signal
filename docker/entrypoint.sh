0cc58ac78e1a2be29376fb36aada559555899d4b
poppyschmo <poppyschmo@users.noreply.github.com>
2018-07-04 23:53:12 -0700

    Remove bash dependency from signal-cli container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,37 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
 # This file is part of ZNC-Signal. See NOTICE for details.
 # License: Apache 2.0 <http://www.apache.org/licenses/LICENSE-2.0>
 
 
 rm -f /etc/supervisord.d/*.conf
-[[ $DEBUG_SUPER ]] &&
+[ "$DEBUG_SUPER" ] &&
     ln -s /usr/local/share/supervisord/*.conf /etc/supervisord.d
 
-if (( ! $# )) && [[ ${0##*/} == interact ]]; then
-    set -- su -l -s /bin/bash signal-cli
+if [ $# -eq 0 ] && [ "${0##*/}" = interact ]; then
+    set -- su -l -s /bin/sh signal-cli
 fi
 
-if (( ! $# )); then
+if [ $# -eq 0 ]; then
     : "${SIGNAL_CLI_CONFIG:=$SIGNAL_CLI_HOME/.config/signal}"
-    if [[ $SIGNAL_CLI_USERNAME && -e $SIGNAL_CLI_CONFIG ]]; then
+    if [ "$SIGNAL_CLI_USERNAME" ] && [ -e "$SIGNAL_CLI_CONFIG" ]; then
         chown -R signal-cli: "$SIGNAL_CLI_HOME" "$SIGNAL_CLI_CONFIG"
-        #                                      shellcheck disable=SC2086,SC2163
-        export ${!SIGNAL_*}
-        cmdline=(
-            /usr/bin/supervisord
-            --nodaemon --pidfile /var/run/supervisord.pid
+        export SIGNAL_CLI_HOME \
+            SIGNAL_CLI_CONFIG \
+            SIGNAL_CLI_OPTS \
+            SIGNAL_CLI_USERNAME \
+            SIGNAL_CLI_VERSION
+        set -- /usr/bin/supervisord \
+            --nodaemon --pidfile /var/run/supervisord.pid \
             --configuration /etc/supervisord.conf
-        )
-        set -- "${cmdline[@]}"
     else
         dbus-daemon --system
         sleep 0.1
-        set -- bash
+        set -- sh
     fi
-elif ! pgrep -f supervisord &>/dev/null; then
+elif ! pgrep -f supervisord >/dev/null 2>&1; then
     echo "supervisord is not running"
-elif [[ $(supervisorctl status signal-cli) != *@(stop|STOP)* ]]; then
+elif supervisorctl status signal-cli | grep -i stop; then
     # Can't pgrep for signal-cli because it's continually stopped/restarted
     # when various checks fail at startup, e.g., account is 'unregistered'
     supervisorctl stop signal-cli  # exits 0 even if already stopped
@@ -40,4 +40,3 @@ else
 fi >&2
 
 exec "$@"
-
