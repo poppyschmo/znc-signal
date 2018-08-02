f99e47be187a76a710f43fbef4e26b121b3fdb75
poppyschmo <poppyschmo@users.noreply.github.com>
2018-08-02 01:26:56 -0700

    Remove narrower from hook tests

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -3,6 +3,7 @@
 
 import pytest
 from copy import deepcopy
+from collections import namedtuple
 from conftest import signal_stub, signal_stub_debug, all_in
 signal_stub = signal_stub  # quiet linter
 signal_stub_debug = signal_stub_debug
@@ -42,180 +43,49 @@ def test_OnLoad(env_stub):
     # TODO use pseudo terminal to test debug logger (likely requires Linux)
 
 
-# TODO move these data records to a separate file and use script to generate
-# (requires irc3 package and a running instance of ZNC)
-get_rel_params = []
-# OnChanMsg(Nick, Channel, sMessage)
-ocm = {'Nick': {'nick': 'tbo',
-                'ident': 'testbot',
-                'host': 'znc.in',
-                'hostmask': 'tbo!testbot@znc.in'},
-       'Channel': {'name': '#test_chan',
-                   'detached': False},
-       'sMessage': 'Welcome dummy!',
-       'network': {'name': 'testnet',
-                   'away': False,
-                   'client_count': 1},
-       'time': '2018-04-21T01:20:13.751970+00:00'}
-get_rel_params.append(("OnChanMsg", ocm))
+# NOTE: order doesn't (currently) matter for flattened/narrowed hook data
+base_rel = {'body': 'Welcome dummy!',
+            'network': 'testnet',
+            'away': False,
+            'client_count': 1,
+            'nick': 'tbo',
+            'ident': 'testbot',
+            'host': 'znc.in',
+            'hostmask': 'tbo!testbot@znc.in',
+            # Real thing uses datetime object
+            'time': '2018-04-21T01:20:13.751970+00:00'}
 
-# OnChanTextMessage(msg):
-octm = {'msg': {'type': 'Text',
-                'nick': {'nick': 'tbo',
-                         'ident': 'testbot',
-                         'host': 'znc.in',
-                         'hostmask': 'tbo!testbot@znc.in'},
-                'channel': {'name': '#test_chan',
-                            'detached': False},
-                'command': 'PRIVMSG',
-                'params': ('#test_chan', 'Welcome dummy!'),
-                'network': {'name': 'testnet',
-                            'away': False,
-                            'client_count': 1},
-                'target': '#test_chan',
-                'text': 'Welcome dummy!'},
-        'time': '2018-04-21T01:20:13.751970+00:00'}
-get_rel_params.append(("OnChanTextMessage", octm))
+rels = namedtuple("Rels",
+                  "OnChanTextMessage OnPrivTextMessage")(
+    dict(base_rel,
+         channel='#test_chan',
+         detached=False,
+         context='#test_chan'),
+    dict(base_rel,
+         body="Oi",
+         context="dummy")
+)
 
-# OnPrivMsg(Nick, sMessage)
-opm = deepcopy(ocm)
-del opm["Channel"]
-opm["sMessage"] = "Oi"
-get_rel_params.append(("OnPrivMsg", opm))
-
-# OnPrivTextMessage(msg)
-optm = deepcopy(octm)
-del optm["msg"]["channel"]
-optm["msg"].update({'params': ('dummy', 'Oi'),  # "command" is still "PRIVMSG"
-                    'target': 'dummy',
-                    'text': 'Oi'})
-get_rel_params.append(("OnPrivTextMessage", optm))
-
-# TODO add sub-1.7 analogs, then add to test params
-#
-# OnPrivActionMessage(msg)
-opam = deepcopy(optm)
-opam["msg"].update({'type': 'Action',
-                    'params': ('dummy', '\x01ACTION Oi\x01')})
-
-# OnPrivCTCPMessage(msg)
-opctcpm = deepcopy(opam)
-opctcpm["msg"]["text"] = "ACTION Oi"
-
-
-def get_relevant_legacy(data):
-    """Narrow/flatten normalized data to zone of interest
-
-    At this point, all data/data.msg values should (mostly) be
-    primitives (strings and ints).
-    """
-    from Signal.commonweal import get_first
-    #
-    def narrow(lookups):  # noqa: E306
-        common = {}
-        from collections import MutableMapping
-        for key, cands in lookups.items():
-            wanted = get_first(data, *cands)
-            if not wanted:
-                continue
-            if isinstance(wanted, MutableMapping):
-                _wanted = dict(wanted)
-                if "name" in wanted:
-                    common[key] = _wanted.pop("name")
-                if key in wanted:
-                    common[key] = _wanted.pop(key)
-                if _wanted != wanted:
-                    common.update(_wanted)
-            else:
-                common[key] = wanted
-        return common
-    #
-    # Common lookups
-    rosetta = {
-        # shallow
-        "body": ("text", "sMessage"),
-        # deep
-        "network": ("network", "Network"),
-        "channel": ("channel", "Channel", "sChannel", "Chan", "sChan"),
-        "nick": ("nick", "Nick"),
-    }
-    narrowed = narrow(rosetta)
-    narrowed["context"] = narrowed.get("channel") or narrowed["nick"]
-    return narrowed
-
-
-@pytest.mark.parametrize("hook_name,hook_data", get_rel_params)
-def test_get_relevant(hook_name, hook_data):
-    rel = get_relevant_legacy(hook_data)
-    # NOTE: order doesn't matter for flattened/narrowed hook data
-    base = {"away": False,
-            "client_count": 1,
-            "host": "znc.in",
-            "hostmask": "tbo!testbot@znc.in",
-            "ident": "testbot",
-            "network": "testnet",
-            "nick": "tbo"}
-    if hook_name.startswith("OnChan"):
-        chan_flat = dict(base)
-        chan_flat.update({"channel": "#test_chan",
-                          "body": "Welcome dummy!",
-                          "detached": False,
-                          "context": "#test_chan"})
-        assert rel == chan_flat
-    else:
-        priv_flat = dict(base)
-        priv_flat.update(body="Oi", context="tbo")
-        assert rel == priv_flat
-    #
-    # Simulate converted dict passed to Signal.reckon()
-    if hook_name in ("OnChanTextMessage", "OnPrivTextMessage"):
-        network = hook_data["msg"]["network"]
-        if hook_name == "OnChanTextMessage":
-            channel = hook_data["msg"]["channel"]
-            context = channel["name"]
-        else:
-            context = hook_data["msg"]["nick"]["nick"]
-        src_info = hook_data["msg"]["nick"]
-    elif hook_name in ("OnChanMsg", "OnPrivMsg"):
-        network = hook_data["network"]
-        if hook_name == "OnChanMsg":
-            channel = hook_data["Channel"]
-            context = hook_data["Channel"]["name"]
-        else:
-            context = hook_data["Nick"]["nick"]
-        src_info = hook_data["Nick"]
-    from collections import defaultdict
-    defnul = defaultdict(type(None), rel)
-    assert defnul["network"] == network["name"]
-    assert defnul["away"] == network["away"]
-    assert defnul["client_count"] == network["client_count"]
-    if hook_name.startswith("OnChan"):
-        assert defnul["channel"] == channel["name"]
-        assert defnul["detached"] == channel["detached"]
-    assert defnul["context"] == context
-    assert defnul["nick"] == src_info["nick"]
-    assert defnul["hostmask"] == src_info["hostmask"]
+# NOTE OnPrivActionMessage(msg) and OnChanActionMessage(msg) are exactly the
+# same as their "Text" counterparts above, once narrowed. Normalized inspector
+# output will show: 'type': 'Action', 'params': ('dummy', '\x01ACTION Oi\x01')
+# as the only real differences.
 
 
 def test_reckon(signal_stub_debug):
+    # Simulate converted dict passed to Signal.reckon()
+    from collections import defaultdict
+    defnul = defaultdict(type(None), rels.OnPrivTextMessage)
+    assert defnul["channel"] is None
+    assert defnul["detached"] is None
+    #
     # Load default config
     sig = signal_stub_debug
     sig.manage_config("load")  # same as '[*Signal] select'
     # Quiet "no host" warning
     sig.OnModCommand('update /settings/host localhost')
     # Simulate a single, simple hook case
-    rel = {'away': False,
-           'channel': '#test_chan',
-           "detached": False,
-           'client_count': 1,
-           'body': 'Welcome dummy!',
-           'host': 'znc.in',
-           'hostmask': 'tbo!testbot@znc.in',
-           'ident': 'testbot',
-           'network': 'testnet',
-           'nick': 'tbo',
-           'context': '#test_chan'}
-    assert get_relevant_legacy(octm) == rel
+    rel = rels.OnChanTextMessage
     #
     data_bak = deepcopy(rel)
     conds = sig.config.conditions
